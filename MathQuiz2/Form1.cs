28b3fc6fc7fb34bad86ca84572beb980d05e1157
Tim Thayne <thayneti@byui.edu>
2019-01-15 09:01:21 -0700

    Iniitial commit

--- MathQuiz2/Form1.cs
+++ MathQuiz2/Form1.cs
@@ -16,5 +16,13 @@ namespace MathQuiz2
         {
             InitializeComponent();
         }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TODO: complete the click note
+
+        }
     }
 }
